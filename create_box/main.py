bf02ea974b0fe44c244dc5e38e0423c5e32e531c
Santiago Basulto <santiago.basulto@gmail.com>
2017-02-18 15:34:54 +0000

    First attempt at create_box

--- create_box/main.py
+++ create_box/main.py
@@ -1,14 +1,9 @@
 """This is the entry point of the program."""
 
-CHEATER_BOX = """****
-****
-****
-"""
-
 
 def create_box(height, width, character):
-    return CHEATER_BOX
-
+    return "@\n"
 
 if __name__ == '__main__':
-    create_box(3, 4, '*')
+    print(create_box(3, 4, '*'))
+    #print("Hello World")
